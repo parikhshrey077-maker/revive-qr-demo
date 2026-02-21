1d8e6176c771b7132f09da94417af13da1932755
[Shrey Parikh] <[parikhshrey077@gmail.com]>
2026-02-21 20:29:58 +0530

    Add home page

--- revive_demo/urls.py
+++ revive_demo/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from lights import views
+import lights.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda r: HttpResponse("🌟 ReVive QR Demo LIVE! Scan QR → LightPassport instant! ☀️")), 
+    path('', lambda r: HttpResponse("🌟 ReVive QR Demo LIVE!<br>Scan: /qr/LED-4567/")), 
     path('light/<str:light_id>/light_passport.html', views.light_passport, name='light_passport'),
     path('qr/<str:light_id>/', views.generate_qr, name='generate_qr'),
 ]
+
